fe15f6a319e6e903d496a8a895e825ee2495bf04
amabbymrtnz <amabigailmrtnz@gmail.com>
2023-05-07 11:51:16 -0600

    Trying to fix Glitch errors - 3000 instead of 3500

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -3,7 +3,7 @@ const whitelist = [
     //glitch site
     'https://dazzling-snickerdoodle-777101.netlify.app', 
     'http://127.0.0.1:5500', 
-    'http://localhost:3500'
+    'http://localhost:3000'
 ];
 //Cors options
 const corsOptions = {
